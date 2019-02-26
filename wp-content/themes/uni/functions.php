fb48c7e26012f85cb174d5358a7669f6390a33cf
Shabaeva <38494580+Shabaeva@users.noreply.github.com>
2019-02-26 19:14:18 +0800

    Перенос статической верстки в ВП

---
+++ wp-content/themes/uni/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+function add_theme_scripts(){
+    wp_enqueue_style('custom-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('style', get_stylesheet_uri());
+    wp_enqueue_script('script', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+   
+    
+}
+add_action('wp_enqueue_scripts', 'add_theme_scripts');
+
+
+
+
+
+
+?>
