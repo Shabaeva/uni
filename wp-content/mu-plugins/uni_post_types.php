29d4d29c093edd496bba8670b8983972777b927a
Shabaeva <38494580+Shabaeva@users.noreply.github.com>
2019-04-08 20:47:02 +0800

    Campus type 1, refactoring

--- wp-content/mu-plugins/uni_post_types.php
+++ wp-content/mu-plugins/uni_post_types.php
@@ -2,6 +2,27 @@
 
 function uni_post_types(){
 
+    //Campus Post type
+register_post_type('campus', array(   
+'rewrite' => array(
+'slug' => 'campuses'
+),
+    'supports' => array('title', 'editor', 'excerpt'),
+    'has_archive' => true,
+    'public'=> true,
+    'labels'=> array(
+        'name'=>'Campuses',
+        'add_new_item'=>'Add New Campus',
+        'edit_item'=>'Edit Campus',
+        'all_items'=>'All Campuses',
+        'singular_name'=>'Campus'
+    ),
+    'menu_icon'=>'dashicons-location-alt'
+    
+    
+
+
+));
 	//Event Post type
 register_post_type('event', array(   
 'rewrite' => array(
